eda7a26b4582cfbf23590362e3e1dfc4158ae8c5
gergsandoval <germansandoval.it@gmail.com>
2023-02-12 22:07:47 -0300

    tidy up methods with many parameters

--- features/steps/user_steps.py
+++ features/steps/user_steps.py
@@ -17,15 +17,29 @@ def when_i_add_a_new_user(context):
     list_service = ListUserService(context)
     list_service.click_add_user()
     row = context.table[0]
-    add_service.add_user(first_name=row['first_name'], last_name=row['last_name'], user_name=row['user_name'], password=row["password"],
-                    customer=row['customer'], role=row['role'], email=row["email"], cell_phone=row['cellphone'])
+    add_service.add_user(
+        first_name=row['first_name'],
+        last_name=row['last_name'],
+        user_name=row['user_name'], 
+        password=row["password"],
+        customer=row['customer'], 
+        role=row['role'], 
+        email=row["email"], 
+        cell_phone=row['cellphone'])
 
 @Then("I see the user has been added to the table")
 def then_i_see_the_user_has_been_added_to_the_table(context):
     list_service = ListUserService(context)
     row = context.table[0]
-    expected_user = ListUser(first_name=row['first_name'], last_name=row['last_name'], user_name=row['user_name'],
-                    customer=row['customer'], role=row['role'], email=row["email"], cell_phone=row['cellphone'], locked=StringHelper.string_to_bool(row["locked"]))
+    expected_user = ListUser(
+        first_name=row['first_name'], 
+        last_name=row['last_name'],
+        user_name=row['user_name'],
+        customer=row['customer'],
+        role=row['role'],
+        email=row["email"],
+        cell_phone=row['cellphone'],
+        locked=StringHelper.string_to_bool(row["locked"]))
     actual_user = list_service.get_actual_user(expected_user.user_name)
     assert expected_user == actual_user
 
